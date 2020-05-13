8bcfdb38e0164995d123ca7881978469ae364923
Yonnys Quintero <leafar.qr@gmail.com>
2020-05-13 13:12:08 -0400

    administrable

--- wp-content/themes/Livorno-master/partials/gallery-one.php
+++ wp-content/themes/Livorno-master/partials/gallery-one.php
@@ -11,105 +11,78 @@
 
 
 						<div class="grid-slide">
-							<div class="grid-row">
-								<div>
-									<img onclick="openModal2();currentSlide2(1)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_11.png" alt="" />
+							<?php $args = array( 'post_type' => 'galeriaExterior','posts_per_page' => 4);?>   
+							<?php $loop = new WP_Query( $args ); 
+							$cout1 = 1;
+							?>
+							<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+								<div class="grid-row">
+									<div>
+										<img onclick="openModal2();currentSlide2(<?php echo $cout1 ?>)" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" />
+									</div>
+									
 								</div>
-								<div>
-									<img onclick="openModal2();currentSlide2(2)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_12.png" alt="" />
-								</div>
-								<div>
-									<img onclick="openModal2();currentSlide2(3)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_13.png" alt="" />
-								</div>
-								<div>
-									<img onclick="openModal2();currentSlide2(4)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_14.png" alt="" />
-								</div>
+								<?php $cout1++; endwhile; ?>
 							</div>
+							<div class="grid-slide">
+								<?php $args = array( 'post_type' => 'galeriaExterior','posts_per_page' => 4);?>   
+								<?php $loop = new WP_Query( $args ); 
+								$cout1 = 1;
+								?>
+								<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+									<div class="grid-row">
+										<div>
+											<img onclick="openModal2();currentSlide2(<?php echo $cout1 ?>)" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" />
+										</div>
 
+									</div>
+									<?php $cout1++; endwhile; ?>
+
+								</div>
+
+								<div class="barra-progress">
+									<div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+										<span class="slider__label sr-only">
+
+										</div>
+										<span class="line"></span>
+									</div>
+								</div>
+							</sectio>
 						</div>
-						<div class="grid-slide">
-							<div class="grid-row">
-								<img onclick="openModal2();currentSlide2(5)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_14.png" alt="" />
-								<img onclick="openModal2();currentSlide2(6)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_13.png" alt="" />
-								<img onclick="openModal2();currentSlide2(7)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_13.png" alt="" />
 
-								<img onclick="openModal2();currentSlide2(8)" src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_11.png" alt="" />
 
-							</div>
-
-						</div>
-
-						<div class="barra-progress">
-							<div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-								<span class="slider__label sr-only">
-
-							</div>
-							<span class="line"></span>
-						</div>
 					</div>
-					</sectio>
+				</section>
+
+
+
+				<!--Modal galeria-->
+				<div id="myModal2" class="modal">
+
+
+					<div class="modal-content">
+						<button aria-label='Close' class='close' data-dismiss='modal' type='button' onclick="closeModal2()">
+							<span aria-hidden='true' class='fa fa-close'></span>
+						</button>
+						<?php $args = array( 'post_type' => 'galeriaExterior','posts_per_page' => 4);?>   
+						<?php $loop = new WP_Query( $args ); 
+						$cout2 = 4;
+						?>
+						<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+							<div class="mySlides2">
+								<!-- <div class="numbertext">1 / 4</div> -->
+								<img src="<?php echo get_the_post_thumbnail_url(); ?>" style="width:100%">
+							</div>
+						<?php endwhile; ?>
+
+
+
+
+						<a class="prev" onclick="plusSlides2(-1)">&#10094;</a>
+						<a class="next" onclick="plusSlides2(1)">&#10095;</a>
+
+
+
+					</div>
 				</div>
-
-
-			</div>
-	</section>
-
-
-
-		<!--Modal galeria-->
-<div id="myModal2" class="modal">
-
-
-	<div class="modal-content">
-		<button aria-label='Close' class='close' data-dismiss='modal' type='button' onclick="closeModal2()">
-			<span aria-hidden='true' class='fa fa-close'></span>
-		</button>
-	
-		<div class="mySlides2">
-			<!-- <div class="numbertext">1 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_11.png" style="width:100%">
-		</div>
-	
-		<div class="mySlides2">
-			<!-- <div class="numbertext">2 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_12.png" style="width:100%">
-		</div>
-	
-		<div class="mySlides2">
-			<!-- <div class="numbertext">3 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_13.png" style="width:100%">
-		</div>
-	
-		<div class="mySlides2">
-			<!-- <div class="numbertext">1 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_14.png" style="width:100%">
-		</div>
-	
-		<div class="mySlides2">
-			<!-- <div class="numbertext">2 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_14.png" style="width:100%">
-		</div>
-	
-		<div class="mySlides2">
-			<!-- <div class="numbertext">3 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_13.png" style="width:100%">
-		</div>
-		<div class="mySlides2">
-			<!-- <div class="numbertext">1 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_12.png" style="width:100%">
-		</div>
-	
-		<div class="mySlides2">
-			<!-- <div class="numbertext">2 / 4</div> -->
-			<img src="<?php echo get_template_directory_uri();?>/assets/img/galeria/image_11.png" style="width:100%">
-		</div>
-	
-	
-	
-		<a class="prev" onclick="plusSlides2(-1)">&#10094;</a>
-		<a class="next" onclick="plusSlides2(1)">&#10095;</a>
-	
-	
-	
-	</div>
-	</div>
